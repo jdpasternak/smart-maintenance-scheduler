53865495ef93052f6bb39eccdc10dc55b6eaebb7
Jake Pasternak <jdp.pasternak@gmail.com>
2025-03-31 12:21:10 -0400

    #64 : adds some starter components to the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,55 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-roboto-mono)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-2xl">Welcome to Smart Maintenance Scheduler</h1>
 
-        <div className="flex items-center flex-col w-full">
-          <a
-            className="rounded-full border border-solid border-black/[.08] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="#"
-          >
-            Get Started
-          </a>
+        <div className="flex items-center flex-col w-full gap-5">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant={"default"}>Login</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>Login</DialogTitle>
+            </DialogContent>
+          </Dialog>
+        </div>
+
+        <div className="flex items-center flex-col w-full gap-5">
+          <Button variant={"default"}>Default</Button>
+          <Button variant={"outline"}>Outline</Button>
+          <Button variant={"ghost"}>Ghost</Button>
+          <Button variant={"destructive"}>Destructive</Button>
+        </div>
+
+        <div className="flex items-center flex-col w-full gap-5">
+          <Card>
+            <CardHeader>
+              <CardTitle>Dashboard</CardTitle>
+            </CardHeader>
+            <CardContent>
+              Welcome to your admin portal
+            </CardContent>
+          </Card>
+        </div>
+
+        <div className="flex items-center flex-col w-full gap-5">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Open</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>Hi!</DialogTitle>
+              <p>Hello from the dialog!</p>
+            </DialogContent>
+          </Dialog>
         </div>
       </main>
-    </div>
+    </div >
   );
 }
