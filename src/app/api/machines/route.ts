801300a16736d4543db967c4f005e84d183b2717
Jake Pasternak <jdp.pasternak@gmail.com>
2025-04-04 09:49:03 -0400

    #85 : updates /api/machines GET route handler to use filterRequest

--- src/app/api/machines/route.ts
+++ src/app/api/machines/route.ts
@@ -1,15 +1,15 @@
+import { Machine } from '@prisma/client';
 import { NextResponse } from 'next/server';
 import { auth } from '@/lib/auth';
 import { filterRequest } from '@/lib/filter-request';
 import { handleApiError } from '@/lib/handle-api-error';
-import { NextAuthRequest } from '@/lib/interfaces';
 import { prisma } from '@/lib/prisma';
 
-export const GET = auth(async (request: NextAuthRequest): Promise<NextResponse> => {
+export const GET = auth(async (req: Request) => {
   try {
-    filterRequest(request);
-    const machineList = await prisma.machine.findMany();
-    return NextResponse.json(machineList);
+    filterRequest(req);
+    const machineList: Machine[] = await prisma.machine.findMany();
+    return NextResponse.json(machineList, { status: 200 });
   } catch (error) {
     return handleApiError(error);
   }
