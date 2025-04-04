567478e71facb6a0655a948fe030e2d3a128b283
Jake Pasternak <jdp.pasternak@gmail.com>
2025-04-04 09:48:33 -0400

    #85 : modifies filterRequest to take Request

--- src/lib/filter-request.ts
+++ src/lib/filter-request.ts
@@ -2,8 +2,8 @@ import { User } from '@prisma/client';
 import { UnauthenticatedRequestError } from './errors';
 import { NextAuthRequest } from './interfaces';
 
-export const filterRequest = (request: NextAuthRequest): User => {
-  const user = request.auth?.user as User | undefined;
+export const filterRequest = (req: Request): User => {
+  const user = (req as NextAuthRequest).auth?.user as User | undefined;
 
   if (!user) {
     throw new UnauthenticatedRequestError();
