8ce197c725ae3d4ec95135d355cc1dedf9b8b141
Jake Pasternak <jdp.pasternak@gmail.com>
2025-04-11 09:41:05 -0400

    fixes typo

---
+++ prisma/migrations/20250411133751_fixes_maintenance_interval_unit_typo/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `maintananceIntervalUnit` on the `Machine` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Machine" DROP COLUMN "maintananceIntervalUnit",
+ADD COLUMN     "maintenanceIntervalUnit" "MaintenanceIntervalUnit";
