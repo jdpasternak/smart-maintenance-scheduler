70e40a43435d1613207faec81575e18b08521a5e
Jake Pasternak <jdp.pasternak@gmail.com>
2025-04-11 09:22:09 -0400

    #25 : adds POST endpoint for  creating machines, adds toast

--- src/app/api/machines/route.ts
+++ src/app/api/machines/route.ts
@@ -16,3 +16,47 @@ export const GET = async (req: Request) => {
     return handleApiError(error);
   }
 };
+
+export const POST = async (req: Request) => {
+  try {
+    const session = await auth();
+    filter(session);
+    const body = await req.json();
+    console.log(body);
+    const newMachine: Machine = await prisma.machine.create({
+      data: body,
+    });
+    return NextResponse.json(newMachine, { status: 201 });
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
+export const PUT = async (req: Request) => {
+  try {
+    const session = await auth();
+    filter(session);
+    const body = await req.json();
+    const updatedMachine: Machine = await prisma.machine.update({
+      where: { id: body.id },
+      data: body,
+    });
+    return NextResponse.json(updatedMachine, { status: 200 });
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
+export const DELETE = async (req: Request) => {
+  try {
+    const session = await auth();
+    filter(session);
+    const body = await req.json();
+    const deletedMachine: Machine = await prisma.machine.delete({
+      where: { id: body.id },
+    });
+    return NextResponse.json(deletedMachine, { status: 200 });
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
